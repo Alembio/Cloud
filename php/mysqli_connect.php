58664b5c88d450b7b3ad82a80d673a8b1edcd5d2
Alembio <alessiogdanz@gmail.com>
2017-05-22 16:10:56 -0500

    Error ortografico

--- php/mysqli_connect.php
+++ php/mysqli_connect.php
@@ -2,7 +2,7 @@
 
 $dbc = new mysqli("localhost","root","","proyecto"); //Guardo la conección a la base de datos MYSQL como una variable
 
-if ($dbc->connect_error) //Si la concección tiene un error
+if ($dbc->connect_error) //Si la conexión tiene un error
 {
   die("Error: " . $dbc->connect_error); //Imprime el error
 }
