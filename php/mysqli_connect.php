c0cc8087ff963d90d616b95f883da27bb2c5d954
Alembio <alessiogdanz@gmail.com>
2017-05-22 16:01:26 -0500

    comenatrios mysqli_connect.php

--- php/mysqli_connect.php
+++ php/mysqli_connect.php
@@ -1,12 +1,12 @@
 <?php
 
-$dbc = new mysqli("localhost","root","","proyecto");
+$dbc = new mysqli("localhost","root","","proyecto"); //Guardo la conección a la base de datos MYSQL como una variable
 
-if ($dbc->connect_error)
+if ($dbc->connect_error) //Si la concección tiene un error
 {
-  die("Error: " . $dbc->connect_error);
+  die("Error: " . $dbc->connect_error); //Imprime el error
 }
-else{
+else{ //Si no, no hagas nada
 
 }
 
