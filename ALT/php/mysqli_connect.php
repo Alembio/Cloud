44776c84b69057e104773d444cea048acb9a3855
Nadnus <fernandodelosheros@gmail.com>
2017-05-24 18:42:19 -0500

    Puse las fotos y roches

---
+++ ALT/php/mysqli_connect.php
@@ -0,0 +1,13 @@
+<?php
+
+$dbc = new mysqli("localhost","root","","proyecto"); //Guardo la conexión a la base de datos MYSQL como una variable
+
+if ($dbc->connect_error) //Si la conexión tiene un error
+{
+  die("Error: " . $dbc->connect_error); //Imprime el error
+}
+else{ //Si no, no hagas nada
+
+}
+
+?>
